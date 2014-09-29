eea78eeea1ef0d37ea6bf885076fdf3656beb9a1
Olivier <alt-oli@hotmail.com>
2014-09-29 12:02:43 +0200

    Altes Login File

--- index.php
+++ index.php
@@ -3,33 +3,29 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
     <html>
         <head>
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <meta name="author" content="Philippe Ruoss, Olivier Alther" />
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="author" content="Philippe Ruoss" />
             <meta name="Keywords" content="" />
             <meta name="Description" content="" />
-                <meta http-equiv="refresh" content="; url=user.php" />
-                <title>Browsergame | Strategia Imperialis | Login</title>
-
-            <link href="css/bootstrap.min.css" rel="stylesheet">
-            <link href="css/main.css" rel="stylesheet">
+            <link rel="stylesheet" type="text/css" href="style.css">
+                <title>Login Strategia Imperialis</title>
         </head>
-    <body>
-        <div id="fullscreen_bg" class="fullscreen_bg"/>
+        <body>
 
-            <div class="container">
+            <form action="login.php" method="post" class="login">
+                <dl>
+                    <label for="login">Benuzername:</label>
+                    <dd><input type="text" name="username" placeholder=""/></dd>
+                    <label for="password">Passwort:</label>
+                    <dd><input type="password" name="password" placeholder=""/></dd>
+                </dl>
 
-                <form class="form-signin">
-                    <h1 class="form-signin-heading text-muted">Anmelden</h1>
-                    <input type="text" class="form-control" placeholder="Email address" required="" autofocus="">
-                    <input type="password" class="form-control" placeholder="Password" required="">
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">
-                        Sign In
-                    </button>
-                </form>
-
-            </div>
-        </div>
-    </body>
-</html>
+                <p>
+                    <input type="submit" name="submit" value="Login" />
+                    <input type="reset" value="Zurücksetzen" /><br>
+                    <a href="registrieren.php">Noch nicht registriert?</a>
+                </p>
+                
+            </form>
+        </body>
+    </html>
